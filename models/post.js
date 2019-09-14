bd6a6fc6e57d6d496eb6a0333819b1ae47494b8b
Jesse McKinney <jessman51386@gmail.com>
2019-09-14 12:07:57 -0400

    yay

---
+++ models/post.js
@@ -0,0 +1,31 @@
+module.exports = function(sequelize, DataTypes) {
+  var Post = sequelize.define("Post", {
+    PosterID: DataTypes.INTEGER,
+    Task: DataTypes.TEXT,
+    Accepted: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    AccepterID: DataTypes.INTEGER,
+    Completed: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  });
+
+  Post.associate = function (models) {
+    Post.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  //   Post.create({
+  //     PosterID: 1,
+  //     Task: "Try me",
+  //     AccepterID: 2
+  //   });
+
+  return Post;
+};
