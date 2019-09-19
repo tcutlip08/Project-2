6ac7ec61c8a00dfa286011c3096f2818a88c346c
Jesse McKinney <jessman51386@gmail.com>
2019-09-18 21:17:07 -0400

    trying another push with the new modified code that dumb template put through... HELLgit add .!

--- public/html/template/js/jquery.easing.1.3.js
+++ public/html/template/js/jquery.easing.1.3.js
@@ -40,74 +40,74 @@ jQuery.easing.jswing = jQuery.easing.swing;
 
 jQuery.extend(jQuery.easing, {
   def: "easeOutQuad",
-  swing: function(x, t, b, c, d) {
+  swing: function (x, t, b, c, d) {
     //alert(jQuery.easing.default);
     return jQuery.easing[jQuery.easing.def](x, t, b, c, d);
   },
-  easeInQuad: function(x, t, b, c, d) {
+  easeInQuad: function (x, t, b, c, d) {
     return c * (t /= d) * t + b;
   },
-  easeOutQuad: function(x, t, b, c, d) {
+  easeOutQuad: function (x, t, b, c, d) {
     return -c * (t /= d) * (t - 2) + b;
   },
-  easeInOutQuad: function(x, t, b, c, d) {
+  easeInOutQuad: function (x, t, b, c, d) {
     if ((t /= d / 2) < 1) {
       return (c / 2) * t * t + b;
     }
     return (-c / 2) * (--t * (t - 2) - 1) + b;
   },
-  easeInCubic: function(x, t, b, c, d) {
+  easeInCubic: function (x, t, b, c, d) {
     return c * (t /= d) * t * t + b;
   },
-  easeOutCubic: function(x, t, b, c, d) {
+  easeOutCubic: function (x, t, b, c, d) {
     return c * ((t = t / d - 1) * t * t + 1) + b;
   },
-  easeInOutCubic: function(x, t, b, c, d) {
+  easeInOutCubic: function (x, t, b, c, d) {
     if ((t /= d / 2) < 1) {
       return (c / 2) * t * t * t + b;
     }
     return (c / 2) * ((t -= 2) * t * t + 2) + b;
   },
-  easeInQuart: function(x, t, b, c, d) {
+  easeInQuart: function (x, t, b, c, d) {
     return c * (t /= d) * t * t * t + b;
   },
-  easeOutQuart: function(x, t, b, c, d) {
+  easeOutQuart: function (x, t, b, c, d) {
     return -c * ((t = t / d - 1) * t * t * t - 1) + b;
   },
-  easeInOutQuart: function(x, t, b, c, d) {
+  easeInOutQuart: function (x, t, b, c, d) {
     if ((t /= d / 2) < 1) {
       return (c / 2) * t * t * t * t + b;
     }
     return (-c / 2) * ((t -= 2) * t * t * t - 2) + b;
   },
-  easeInQuint: function(x, t, b, c, d) {
+  easeInQuint: function (x, t, b, c, d) {
     return c * (t /= d) * t * t * t * t + b;
   },
-  easeOutQuint: function(x, t, b, c, d) {
+  easeOutQuint: function (x, t, b, c, d) {
     return c * ((t = t / d - 1) * t * t * t * t + 1) + b;
   },
-  easeInOutQuint: function(x, t, b, c, d) {
+  easeInOutQuint: function (x, t, b, c, d) {
     if ((t /= d / 2) < 1) {
       return (c / 2) * t * t * t * t * t + b;
     }
     return (c / 2) * ((t -= 2) * t * t * t * t + 2) + b;
   },
-  easeInSine: function(x, t, b, c, d) {
+  easeInSine: function (x, t, b, c, d) {
     return -c * Math.cos((t / d) * (Math.PI / 2)) + c + b;
   },
-  easeOutSine: function(x, t, b, c, d) {
+  easeOutSine: function (x, t, b, c, d) {
     return c * Math.sin((t / d) * (Math.PI / 2)) + b;
   },
-  easeInOutSine: function(x, t, b, c, d) {
+  easeInOutSine: function (x, t, b, c, d) {
     return (-c / 2) * (Math.cos((Math.PI * t) / d) - 1) + b;
   },
-  easeInExpo: function(x, t, b, c, d) {
+  easeInExpo: function (x, t, b, c, d) {
     return t === 0 ? b : c * Math.pow(2, 10 * (t / d - 1)) + b;
   },
-  easeOutExpo: function(x, t, b, c, d) {
+  easeOutExpo: function (x, t, b, c, d) {
     return t === d ? b + c : c * (-Math.pow(2, (-10 * t) / d) + 1) + b;
   },
-  easeInOutExpo: function(x, t, b, c, d) {
+  easeInOutExpo: function (x, t, b, c, d) {
     if (t === 0) {
       return b;
     }
@@ -119,19 +119,19 @@ jQuery.extend(jQuery.easing, {
     }
     return (c / 2) * (-Math.pow(2, -10 * --t) + 2) + b;
   },
-  easeInCirc: function(x, t, b, c, d) {
+  easeInCirc: function (x, t, b, c, d) {
     return -c * (Math.sqrt(1 - (t /= d) * t) - 1) + b;
   },
-  easeOutCirc: function(x, t, b, c, d) {
+  easeOutCirc: function (x, t, b, c, d) {
     return c * Math.sqrt(1 - (t = t / d - 1) * t) + b;
   },
-  easeInOutCirc: function(x, t, b, c, d) {
+  easeInOutCirc: function (x, t, b, c, d) {
     if ((t /= d / 2) < 1) {
       return (-c / 2) * (Math.sqrt(1 - t * t) - 1) + b;
     }
     return (c / 2) * (Math.sqrt(1 - (t -= 2) * t) + 1) + b;
   },
-  easeInElastic: function(x, t, b, c, d) {
+  easeInElastic: function (x, t, b, c, d) {
     var s = 1.70158;
     var p = 0;
     var a = c;
@@ -159,7 +159,7 @@ jQuery.extend(jQuery.easing, {
       ) + b
     );
   },
-  easeOutElastic: function(x, t, b, c, d) {
+  easeOutElastic: function (x, t, b, c, d) {
     var s = 1.70158;
     var p = 0;
     var a = c;
@@ -185,7 +185,7 @@ jQuery.extend(jQuery.easing, {
       b
     );
   },
-  easeInOutElastic: function(x, t, b, c, d) {
+  easeInOutElastic: function (x, t, b, c, d) {
     var s = 1.70158;
     var p = 0;
     var a = c;
@@ -206,58 +206,60 @@ jQuery.extend(jQuery.easing, {
       var s = (p / (2 * Math.PI)) * Math.asin(c / a);
     }
     if (t < 1) {
-      {return (
-        -0.5 *
+      {
+        return (
+          -0.5 *
           (a *
             Math.pow(2, 10 * (t -= 1)) *
             Math.sin(((t * d - s) * (2 * Math.PI)) / p)) +
-        b
-      );}
-    return (
-      a *
+          b
+        );
+      }
+      return (
+        a *
         Math.pow(2, -10 * (t -= 1)) *
         Math.sin(((t * d - s) * (2 * Math.PI)) / p) *
         0.5 +
-      c +
-      b
-    );
-  },
-  easeInBack: function(x, t, b, c, d, s) {
-    if (s == undefined) s = 1.70158;
-    return c * (t /= d) * t * ((s + 1) * t - s) + b;
-  },
-  easeOutBack: function(x, t, b, c, d, s) {
-    if (s == undefined) s = 1.70158;
-    return c * ((t = t / d - 1) * t * ((s + 1) * t + s) + 1) + b;
-  },
-  easeInOutBack: function(x, t, b, c, d, s) {
-    if (s == undefined) s = 1.70158;
-    if ((t /= d / 2) < 1)
-      return (c / 2) * (t * t * (((s *= 1.525) + 1) * t - s)) + b;
-    return (c / 2) * ((t -= 2) * t * (((s *= 1.525) + 1) * t + s) + 2) + b;
-  },
-  easeInBounce: function(x, t, b, c, d) {
-    return c - jQuery.easing.easeOutBounce(x, d - t, 0, c, d) + b;
-  },
-  easeOutBounce: function(x, t, b, c, d) {
-    if ((t /= d) < 1 / 2.75) {
-      return c * (7.5625 * t * t) + b;
-    } else if (t < 2 / 2.75) {
-      return c * (7.5625 * (t -= 1.5 / 2.75) * t + 0.75) + b;
-    } else if (t < 2.5 / 2.75) {
-      return c * (7.5625 * (t -= 2.25 / 2.75) * t + 0.9375) + b;
-    } else {
-      return c * (7.5625 * (t -= 2.625 / 2.75) * t + 0.984375) + b;
+        c +
+        b
+      );
+    },
+    easeInBack: function(x, t, b, c, d, s) {
+      if (s == undefined) s = 1.70158;
+      return c * (t /= d) * t * ((s + 1) * t - s) + b;
+    },
+    easeOutBack: function(x, t, b, c, d, s) {
+      if (s == undefined) s = 1.70158;
+      return c * ((t = t / d - 1) * t * ((s + 1) * t + s) + 1) + b;
+    },
+    easeInOutBack: function(x, t, b, c, d, s) {
+      if (s == undefined) s = 1.70158;
+      if ((t /= d / 2) < 1)
+        return (c / 2) * (t * t * (((s *= 1.525) + 1) * t - s)) + b;
+      return (c / 2) * ((t -= 2) * t * (((s *= 1.525) + 1) * t + s) + 2) + b;
+    },
+    easeInBounce: function(x, t, b, c, d) {
+      return c - jQuery.easing.easeOutBounce(x, d - t, 0, c, d) + b;
+    },
+    easeOutBounce: function(x, t, b, c, d) {
+      if ((t /= d) < 1 / 2.75) {
+        return c * (7.5625 * t * t) + b;
+      } else if (t < 2 / 2.75) {
+        return c * (7.5625 * (t -= 1.5 / 2.75) * t + 0.75) + b;
+      } else if (t < 2.5 / 2.75) {
+        return c * (7.5625 * (t -= 2.25 / 2.75) * t + 0.9375) + b;
+      } else {
+        return c * (7.5625 * (t -= 2.625 / 2.75) * t + 0.984375) + b;
+      }
+    },
+    easeInOutBounce: function(x, t, b, c, d) {
+      if (t < d / 2)
+        return jQuery.easing.easeInBounce(x, t * 2, 0, c, d) * 0.5 + b;
+      return (
+        jQuery.easing.easeOutBounce(x, t * 2 - d, 0, c, d) * 0.5 + c * 0.5 + b
+      );
     }
-  },
-  easeInOutBounce: function(x, t, b, c, d) {
-    if (t < d / 2)
-      return jQuery.easing.easeInBounce(x, t * 2, 0, c, d) * 0.5 + b;
-    return (
-      jQuery.easing.easeOutBounce(x, t * 2 - d, 0, c, d) * 0.5 + c * 0.5 + b
-    );
-  }
-});
+  });
 
 /*
  *
